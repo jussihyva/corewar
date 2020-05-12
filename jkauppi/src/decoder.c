90012d0fc3692b5575b1c9d5dd91a70756210ce7
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-05-12 16:14:20 +0300

    Leak issues are fixed.

--- jkauppi/src/decoder.c
+++ jkauppi/src/decoder.c
@@ -6,28 +6,38 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 11:28:48 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/05/12 11:37:20 by ubuntu           ###   ########.fr       */
+/*   Updated: 2020/05/12 16:11:12 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "decoder.h"
 
-int					main(int argc, char **argv)
+static t_input		*read_input_data(int *argc, char ***argv)
 {
 	int			fd;
 	t_input		*input;
 
-	ft_step_args(&argc, &argv);
+	ft_step_args(argc, argv);
 	input = (t_input *)ft_memalloc(sizeof(input));
-	read_opt(input, &argc, &argv);
+	read_opt(input, argc, argv);
 	read_g_op_tab(input);
 	fd = 0;
 	input->file_content = read_input_file(fd, &input->file_content_size);
+	return (input);
+}
+
+int					main(int argc, char **argv)
+{
+	t_input		*input;
+
+	input = read_input_data(&argc, &argv);
 	if (input->opt & format_asm)
 		print_asm(input, input->file_content, input->file_content_size);
 	else
 		print_hex(input->file_content, input->file_content_size);
 	free(input->file_content);
 	ft_printf("END\n");
+	free(input->g_op_tab);
+//	free(input);
 	return (0);
 }
--- jkauppi/src/memory_1.c
+++ jkauppi/src/memory_1.c
@@ -6,16 +6,37 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/07 19:32:46 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/05/08 00:17:47 by ubuntu           ###   ########.fr       */
+/*   Updated: 2020/05/12 16:02:28 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "decoder.h"
 
-void	del_elem(void *elem, size_t size)
+void			del_elem(void *elem, size_t size)
 {
 	(void)size;
 	free(elem);
 	elem = NULL;
+	return ;
+}
+
+static void		del_asm_code(void *elem, size_t size)
+{
+	t_instruction	*instruction;
+
+	(void)size;
+	instruction = *(t_instruction **)elem;
+	free(instruction);
+	free(elem);
+	elem = NULL;
+	return ;
+}
+
+void			remove_asm_code(t_asm_code *asm_code)
+{
+	ft_lstdel(asm_code->instruction_lst, del_asm_code);
+	free(asm_code->instruction_lst);
+	free(asm_code->header);
+	free(asm_code);
 	return ;
 }

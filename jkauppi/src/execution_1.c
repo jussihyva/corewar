ae2555cb3daf8231ea3fe6b78160bce39f3877fc
Juhani Kauppi <juhanikauppi@hotmail.com>
2020-06-05 14:37:50 +0300

    xor and st instructions are implemented. prel version of fork.

--- jkauppi/src/execution_1.c
+++ jkauppi/src/execution_1.c
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/13 09:10:10 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/06/05 12:02:21 by ubuntu           ###   ########.fr       */
+/*   Updated: 2020/06/05 14:31:11 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,64 +123,6 @@ void			exec_add(t_cpu *cpu, t_instruction *instruction, t_asm_code *asm_code)
 	cpu->reg[instruction->param[2].value] =
 										cpu->reg[instruction->param[0].value] +
 										cpu->reg[instruction->param[1].value];
-	cpu->PC = instruction->start_p + instruction->length;
-	return ;
-}
-
-void			exec_or(t_cpu *cpu, t_instruction *instruction, t_asm_code *asm_code)
-{
-	int			param[2];
-
-	(void)asm_code;
-	ft_bzero(param, sizeof(param));
-	if (instruction->param[0].type == REG_CODE)
-		param[0] = cpu->reg[instruction->param[0].value];
-	if (instruction->param[2].type == REG_CODE)
-		param[1] = cpu->reg[instruction->param[1].value];
-	cpu->reg[instruction->param[2].value] = param[0] | param[1];
-	cpu->PC = instruction->start_p + instruction->length;
-	return ;
-}
-
-void			exec_and(t_cpu *cpu, t_instruction *instruction, t_asm_code *asm_code)
-{
-	int			param[2];
-
-	(void)asm_code;
-	ft_bzero(param, sizeof(param));
-	if (instruction->param[0].type == REG_CODE)
-		param[0] = cpu->reg[instruction->param[0].value];
-	if (instruction->param[2].type == REG_CODE)
-		param[1] = cpu->reg[instruction->param[1].value];
-	cpu->reg[instruction->param[2].value] = param[0] & param[1];
-	cpu->PC = instruction->start_p + instruction->length;
-	return ;
-}
-
-void			exec_sti(t_cpu *cpu, t_instruction *instruction, t_asm_code *asm_code)
-{
-	size_t		i;
-	char		*p;
-
-	(void)asm_code;
-	if (instruction->param[1].type == DIR_CODE && instruction->param[2].type == DIR_CODE)
-	{
-		i = 0;
-		i += instruction->param[1].value;
-		i += instruction->param[2].value;
-		p = cpu->PC + i;
-		p[0] = (cpu->reg[instruction->param[0].value] >> (8 * 3)) * 0xff;
-		p[1] = (cpu->reg[instruction->param[0].value] >> (8 * 2)) * 0xff;
-		p[2] = (cpu->reg[instruction->param[0].value] >> (8 * 1)) * 0xff;
-		p[3] = (cpu->reg[instruction->param[0].value] >> (8 * 0)) * 0xff;
-	}
-	else
-	{
-		ft_printf("%08x: ", instruction->start_p - asm_code->file_content);
-		print_hex_string(0, instruction->start_p, instruction->length);
-		print_params(instruction->param);
-		ft_printf("\n");
-	}
 	cpu->PC = instruction->start_p + instruction->length;
 	return ;
 }

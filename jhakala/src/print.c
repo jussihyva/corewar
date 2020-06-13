b550c9306ef34d2d1a7bcb8b52ded734656d2614
Juha Hakala <jhakala@Juhas-MBP.wlan>
2020-06-13 03:50:27 +0300

    changet line's/cmd's args fro char** to linked list, still need to reverse them

--- jhakala/src/print.c
+++ jhakala/src/print.c
@@ -3,6 +3,8 @@
 
 void	print_line(t_line *new, int j, int mode)
 {
+	t_arg *arg;
+	arg = new->arg;
 	printf("name: '%s', ", g_op_tab[j].str);
 	printf("op_code: '%d', ", new->op_code);
 	printf("n_arg: '%d' ", new->n_arg);
@@ -10,14 +12,15 @@ void	print_line(t_line *new, int j, int mode)
 	{
 		if (mode > 0)
 		{
-			printf("arg[%d] = '%s', ", i, new->arg[i]);
-			printf("t_arg: %d,", g_op_tab[j].t_arg[i]);
+			printf("arg%d = '%s', ", i + 1, arg->str);
+//			printf("t_arg: %d,", g_op_tab[j].t_arg[i]);
 		}
 		else
 		{
-			printf("'%s', ", new->arg[i]);
-			printf("%d, ", g_op_tab[j].t_arg[i]);
+			printf("arg = '%s', ", arg->str);			
+//			printf("%d, ", g_op_tab[j].t_arg[i]);
 		}
+		arg = arg->next;
 	}
 	printf("\n");
 }

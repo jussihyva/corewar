d22d9586c1aa1e30f404314f6fab14d8cafdf3b2
Juha Hakala <hakalan.juha@gmail.com>
2020-06-21 03:31:46 +0300

    error in file or in input line handling for asm

--- jhakala/real_dir/src/init.c
+++ jhakala/real_dir/src/init.c
@@ -6,13 +6,49 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/20 00:15:32 by jhakala           #+#    #+#             */
-/*   Updated: 2020/06/20 02:24:54 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/06/21 03:20:59 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 #include "op.h"
-/*
+
+void	prio(int *op)
+{
+	int i;
+
+	i = 0;
+	while (i < 26)
+		ft_printf("%d", op[i++]);
+	ft_printf("\n");
+}
+
+void	pril(t_label *label)
+{
+	while (label)
+	{
+		ft_printf("'%s'\n", label->name);
+		label = label->next;
+	}
+}
+
+void	pri(t_cmd *cmd)
+{
+	t_arg *arg;
+	
+	while (cmd)
+	{
+		arg = cmd->arg;
+		for (int i = 0; i < cmd->n_arg; i++)
+		{
+			ft_printf("'%s'", arg->str);
+			arg = arg->next;
+		}
+		cmd = cmd->next;
+		ft_printf("\n");
+	}
+}
+
 void	add_label(t_label **alst, t_label *new)
 {
 	new->next = *alst;
@@ -30,20 +66,30 @@ void	add_cmd(t_cmd **alst, t_cmd *new)
 	new->next = *alst;
 	*alst = new;
 }
-*/
-t_champ	*init_champ(int fd)
-{
-	t_champ *champ;
 
+t_champ	*init_champ(int fd, int *op)
+{
+	t_champ	*champ;
+	int		r;
+
+	r = 2;
 	champ = NULL;
 	if (!(champ = (t_champ*)malloc(sizeof(t_champ))))
 		return (NULL);
-	champ->name = get_str(fd, NAME_CMD_STRING);
-	champ->comment = get_str(fd, COMMENT_CMD_STRING);
+	champ->name = get_str(fd, NAME_CMD_STRING, &r);
+	champ->comment = get_str(fd, COMMENT_CMD_STRING, &r);
 	champ->size = 0;
 	champ->label = NULL;
-//	champ->cmd = get_lines(champ, fp);
+	champ->cmd = get_lines(champ, fd, r);
 	ft_printf("champ->name = '%s'\n", champ->name);
 	ft_printf("champ->comment = '%s'\n", champ->comment);
+	if (check_champ(champ) > 0)
+		ft_printf("ERROR\n\n");
+//		free_champ(champ);
+	else
+		ft_printf("OK\n\n");
+	if (op[20] == 1)
+		system("leaks asm");
+	prio(op);
 	return (champ);
 }
---
+++ jhakala/real_dir/src/parse_utils.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 16:28:31 by jhakala           #+#    #+#             */
+/*   Updated: 2020/06/20 19:58:42 by jhakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+#include "op.h"
+
+int		calc_cmd_size(t_cmd *cmd)
+{
+	int		size;
+	int		i;
+	t_arg	*arg;
+
+	i = -1;
+	size = 1;
+	if (cmd->statement_code == 1)
+		size += 1;
+	arg = cmd->arg;
+	while (++i < cmd->n_arg)
+	{
+		if (arg->type == 1)
+			size += 1;
+		else if (arg->type == 2)
+			size += cmd->dir_size;
+		else if (arg->type == 3)
+			size += 2;
+		arg = arg->next;
+	}
+	cmd->size = size;
+	return (size);
+}
+
+int		arg_str_size(char *str, int *i)
+{
+	int len;
+
+	len = 0;
+	if (str[*i] == SEPARATOR_CHAR)
+		(*i)++;
+	*i = skip_whitespace(str, *i);
+	while (str[*i] != ' ' && str[*i] != '	' &&
+		   str[*i] != SEPARATOR_CHAR && str[*i] != '\n' && str[*i])
+	{
+		len++;
+		(*i)++;
+	}
+
+	return (len);
+}
+
+int		arg_reg(t_arg *arg, char *str, int i)
+{
+	int res;
+
+	res = 0;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+		res = res * 10 + (str[i++] - 48);
+	if (str[i] != '\0' || res > REG_NUMBER)
+		arg->error = ARG_ERROR_2;
+	return (res);
+}
+
+int		arg_dir(t_arg *arg, char *str, int i)
+{
+	int res;
+	int minus;
+
+	minus = 1;
+	res = 0;
+	if (str[i] == '-')
+	{
+		minus = -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+		res = res * 10 + (str[i++] - 48);
+	if (str[i] != '\0')
+		arg->error = ARG_ERROR_3;
+	return (res * minus);
+}
+
+char	*arg_dir_label(t_arg *arg, char *str, int i)
+{
+	char	*res;
+	int		j;
+
+	j = 0;
+	res = (char*)malloc(sizeof(char) * (strlen(str) - i));
+	while (str[i])
+		res[j++] = str[i++];
+	res[j] = '\0';
+	if (str[i] != '\0')	
+		arg->error = ARG_ERROR_4;
+	return (res);
+}

7a1bdc3ab2485f545278151c678f853249b661e7
Juha Hakala <hakalan.juha@gmail.com>
2020-06-22 11:56:35 +0300

    real asm ready, checking errors still, mayde some more flags

---
+++ jhakala/real_dir/src/check_name.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_name.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 09:28:42 by jhakala           #+#    #+#             */
+/*   Updated: 2020/06/22 10:52:02 by jhakala          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "asm.h"
+
+int		check_name(int *fd, char *str, int *op, char *end)
+{
+	int i;
+	int j;
+	char *tmp;
+	char *name;
+	
+	if ((j = ft_strlen(str) - 2) > 0 && str[j] == '.' && str[j + 1] == 's')
+	{
+		i = op[22] == 1 ? j - 2 : 0;
+		while (i > 0)
+		{
+			if (str[i] == '/')
+			{
+				i++;
+				break ;
+			}
+			i--;
+		}
+		ft_strlcat((tmp = ft_strnew(j - i + 5)), &(*(str + i)), j - i + 1);
+		*fd = open((name = ft_strjoin(tmp, end)), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		free(tmp);
+		free(name);
+		return (fd > 0 ? 1 : 0);
+	}
+	return (0);
+}

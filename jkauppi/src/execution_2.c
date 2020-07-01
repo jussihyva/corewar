b1a6ac504f3cc8441afc99afa573c6b15a9edec0
Juhani Kauppi <jkauppi@student.hive.fi>
2020-07-01 13:00:00 +0300

    g_op_tab from asm_code structure to cpu structure

--- jkauppi/src/execution_2.c
+++ jkauppi/src/execution_2.c
@@ -6,37 +6,31 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/05 13:47:56 by ubuntu            #+#    #+#             */
-/*   Updated: 2020/06/30 10:36:48 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/07/01 12:45:51 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cpu.h"
 
-void			exec_live(t_cpu *cpu, t_instruction *instruction,
-														t_asm_code *asm_code)
+void			exec_live(t_cpu *cpu, t_instruction *instruction)
 {
-	(void)asm_code;
 	cpu->is_live += 1;
 	cpu->pc = instruction->start_p + instruction->length;
 	ft_printf("Live update %d(%d)\n", cpu->is_live, cpu->reg[1]);
 	return ;
 }
 
-void			exec_fork(t_cpu *cpu, t_instruction *instruction,
-														t_asm_code *asm_code)
+void			exec_fork(t_cpu *cpu, t_instruction *instruction)
 {
-	(void)asm_code;
 	cpu->pc = instruction->start_p + instruction->length;
 	return ;
 }
 
-void			exec_sti(t_cpu *cpu, t_instruction *instruction,
-														t_asm_code *asm_code)
+void			exec_sti(t_cpu *cpu, t_instruction *instruction)
 {
 	size_t		i;
 	char		*p;
 
-	(void)asm_code;
 	i = 0;
 	if (instruction->param[1].type == DIR_CODE)
 		i += instruction->param[1].value;
@@ -65,13 +59,11 @@ void			exec_sti(t_cpu *cpu, t_instruction *instruction,
 	return ;
 }
 
-void			exec_st(t_cpu *cpu, t_instruction *instruction,
-														t_asm_code *asm_code)
+void			exec_st(t_cpu *cpu, t_instruction *instruction)
 {
 	size_t		i;
 	char		*p;
 
-	(void)asm_code;
 	if (instruction->param[1].type == IND_CODE)
 	{
 		i = instruction->param[1].value;
@@ -83,7 +75,8 @@ void			exec_st(t_cpu *cpu, t_instruction *instruction,
 	}
 	else
 	{
-		ft_printf("%08x: ", instruction->start_p - asm_code->file_content);
+		ft_printf("%08x: ", instruction->start_p - cpu->program_start_ptr +
+															sizeof(t_header));
 		print_hex_string(0, instruction->start_p, instruction->length);
 		print_params(instruction->param);
 		ft_printf("\n");

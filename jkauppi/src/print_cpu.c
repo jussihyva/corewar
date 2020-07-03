a51f94331006332ecb3873bea689292455b20717
Juhani Kauppi <jkauppi@student.hive.fi>
2020-07-03 10:58:54 +0300

    Moving parameters from cpu to player struct ongoing.

--- jkauppi/src/print_cpu.c
+++ jkauppi/src/print_cpu.c
@@ -6,13 +6,13 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 15:46:25 by jkauppi           #+#    #+#             */
-/*   Updated: 2020/07/02 14:44:53 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/07/03 10:32:50 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cpu.h"
 
-void			print_cpu(t_cpu *cpu, t_input *input,
+void			print_cpu(t_cpu *cpu, t_input *input, t_player *player,
 							t_instruction *instruction)
 {
 	int			reg;
@@ -22,9 +22,9 @@ void			print_cpu(t_cpu *cpu, t_input *input,
 	ft_printf("%85s", "CPU: ");
 	reg = 0;
 	while (++reg < 8)
-		ft_printf(" r%d=%d", reg, cpu->reg[reg]);
-	ft_printf(" is_live=%d", cpu->is_live);
-	ft_printf(" carry=%d", cpu->carry);
+		ft_printf(" r%d=%d", reg, player->reg[reg]);
+	ft_printf(" is_live=%d", player->is_live);
+	ft_printf(" carry=%d", player->carry);
 	ft_printf(" cycles_to_die=%d", cpu->current_cycle_to_die);
 	ft_printf("\n");
 	return ;

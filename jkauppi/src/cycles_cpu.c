23e72e60fd07dbb5be9467ea304c975b0c4d4da1
Juhani Kauppi <jkauppi@student.hive.fi>
2020-07-06 15:16:38 +0300

    cycle_cnt and next_cycle_to_die_point moved back to cpu structure.

--- jkauppi/src/cycles_cpu.c
+++ jkauppi/src/cycles_cpu.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 15:00:30 by jkauppi           #+#    #+#             */
-/*   Updated: 2020/07/06 14:04:12 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/07/06 15:08:49 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static int			is_player_alive(t_cpu *cpu, t_player *player)
 		cpu->current_number_of_checks = 0;
 	}
 	player->is_live = 0;
-	player->next_cycle_to_die_point = player->cycle_cnt +
+	cpu->next_cycle_to_die_point = cpu->cycle_cnt +
 											cpu->current_cycle_to_die;
 	ft_printf("  Updated cycle to die cnt: %d\n",
 											cpu->current_cycle_to_die);
@@ -44,10 +44,10 @@ int					execute_cycles(int cycles_to_execute, t_cpu *cpu,
 	reason_to_kill = 0;
 	while (cycles_to_execute-- && !reason_to_kill)
 	{
-		player->cycle_cnt++;
-		ft_printf("Cycle : %lld (%lld)\n", player->cycle_cnt,
-						player->next_cycle_to_die_point - player->cycle_cnt);
-		if (player->cycle_cnt == player->next_cycle_to_die_point)
+		cpu->cycle_cnt++;
+		ft_printf("Cycle : %lld (%lld)\n", cpu->cycle_cnt,
+						cpu->next_cycle_to_die_point - cpu->cycle_cnt);
+		if (cpu->cycle_cnt == cpu->next_cycle_to_die_point)
 			reason_to_kill = is_player_alive(cpu, player);
 	}
 	return (reason_to_kill);

7c4ae42f560200fac217b17f5624d2a7397f5036
Juha Hakala <hakalan.juha@gmail.com>
2020-07-22 23:21:50 +0300

    get arguments, and get next statement_code place

--- jhakala/test_dir/src/game/run_game.c
+++ jhakala/test_dir/src/game/run_game.c
@@ -6,7 +6,7 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 17:41:20 by jhakala           #+#    #+#             */
-/*   Updated: 2020/07/20 20:18:54 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/07/22 22:11:49 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,27 @@
 int		cycle_through_c(t_game *game)
 {
 	t_carriage *c;
+	int	size;
 
 	c = game->c_lst;
 	while (c)
 	{
 		if (c->remaining_cycle == 1)
 		{
-			if (!read_statement_code(c, game->arena, c->place))
-			{
-				exit(0);
-//				run_statement;
-			}
-//			else
-//				go_to_next_valid_statement;
+			size = read_statement_code(c, game, c->place);
+			c->place = c->place + size;
+			ft_printf("id'%d'place='%d'\n", c->id, c->place);
 			//check if statement_code is good to go with arguments that are ahead
-			// do the code if possible
-			// else go to next good code
 		}
-		if (c->remaining_cycle == 0)
+		else if (c->remaining_cycle == 0)
 		{
-			// go to next statement_code
-			// put c->remaining_cycle vastaamaan
+			ft_printf("id='%d'place='%d'\n", c->id, c->place);
+			c->statement_code = game->arena[c->place];
+			ft_printf("here\n");
+			if (c->statement_code > 0 && c->statement_code < 17)
+				c->remaining_cycle = g_op_tab[c->statement_code - 1].n_cycles;
+			else
+				c->remaining_cycle = 1;
 		}
 		c->remaining_cycle--;
 		c = c->next;
@@ -54,6 +54,7 @@ int		run_game(t_mem *mem)
 		cycle_through_c(game);
 		if (game->total_cycles == mem->dump)
 		{
+			print_arena(game->arena);
 			ft_printf("printf dump at '%d==%d' cycles\n", game->total_cycles, mem->dump);
 			return (1);
 		}

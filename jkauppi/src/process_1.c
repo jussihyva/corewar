38084e8a45d12d53e463a6a92b11112657b1a3be
Juhani Kauppi <jkauppi@student.hive.fi>
2020-07-24 14:42:26 +0300

    Execution of unknown opcode implemented.

--- jkauppi/src/process_1.c
+++ jkauppi/src/process_1.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:02:28 by jkauppi           #+#    #+#             */
-/*   Updated: 2020/07/23 11:46:44 by jkauppi          ###   ########.fr       */
+/*   Updated: 2020/07/24 14:30:05 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,18 @@
 t_process		*initialize_process(t_cpu *cpu, char *pc)
 {
 	t_process		*process;
+	t_opcode		next_opcode;
 
 	process = (t_process *)ft_memalloc(sizeof(*process));
 	process->process_id = cpu->process_cnt;
 	process->pc = pc;
 	process->program_start_ptr = process->pc;
 	process->reg[1] = -process->process_id;
-	process->next_instruction = parse_instruction(cpu, process->pc);
-	process->cycle_point_for_next_instruction = cpu->cycle_cnt +
-						cpu->g_op_tab[process->next_instruction->opcode].cycles;
+	next_opcode = *process->pc;
+	if (next_opcode > 0 && next_opcode < 17)
+		process->cycle_point_for_next_instruction = cpu->cycle_cnt +
+					cpu->g_op_tab[next_opcode].cycles;
+	else
+		process->cycle_point_for_next_instruction = cpu->cycle_cnt + 1;
 	return (process);
 }

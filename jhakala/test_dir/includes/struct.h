5a69078391d431bc388cd72c060c02b9c3f05b26
Juha Hakala <jhakala@c3r4p3.hive.fi>
2020-07-24 21:55:39 +0300

    cycle check implimented. add, live, sti, zjmp ready?

--- jhakala/test_dir/includes/struct.h
+++ jhakala/test_dir/includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 14:14:20 by jhakala           #+#    #+#             */
-/*   Updated: 2020/07/24 13:31:03 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/07/24 18:42:23 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,14 @@ typedef struct			s_game
 	int					c_nbr;
 	struct s_carriage	*c_lst;
 	char				*arena;
-	t_player			*last_alive;
+	struct s_player			*last_alive;
 	int					total_cycles;
 	int					n_live_in_cycle;
 	int					cycles_to_die;
 	int					n_player;
 	t_player			**players;
 	int					get_die;
+	int					max_check;
 }						t_game;
 
 typedef struct			s_mem

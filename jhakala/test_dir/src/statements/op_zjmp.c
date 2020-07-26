75b06f6012d49df8c5c21d1fe8c16f996e414465
Juha Hakala <jhakala@c3r4p3.hive.fi>
2020-07-26 20:51:59 +0300

    more statements added, just lld, lldi and aff missing

--- jhakala/test_dir/src/statements/op_zjmp.c
+++ jhakala/test_dir/src/statements/op_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:53:27 by jhakala           #+#    #+#             */
-/*   Updated: 2020/07/25 18:21:40 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/07/26 18:43:13 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 int    op_zjmp(t_game *game, int place, t_carriage *c)
 {
 	int res;
+	int val1;
 
 	res = 0;
 	ft_memcpy(&res, &game->arena[place + 1], 2);
 	res = REV_S(res);
 	if (c->carry == 1)
 	{
-		c->place += (res % IDX_MOD) - 3;
-		ft_printf(" P   %d | zjmp %d | OK\n", c->id, res);
+		val1 = (res % IDX_MOD) - 3;
+		if (val1 + c->place >= MEM_SIZE)
+			c->place += val1 - MEM_SIZE;
+		else if (val1 + c->place < 0)
+			c->place += val1 + MEM_SIZE;
+		else
+			c->place += val1;
+		ft_printf(" P   %d | zjmp %d | (0x%04d) | OK\n", c->id, res, c->place);
 	}
 	else
 		ft_printf(" P   %d | zjmp %d | ERROR\n", c->id, res);

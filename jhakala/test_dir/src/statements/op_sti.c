aed040154a26bad196bad156ab159bd986e5a9b6
Juha Hakala <hakalan.juha@gmail.com>
2020-08-06 17:32:16 +0300

    something

--- jhakala/test_dir/src/statements/op_sti.c
+++ jhakala/test_dir/src/statements/op_sti.c
@@ -6,7 +6,7 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:54:08 by jhakala           #+#    #+#             */
-/*   Updated: 2020/08/06 17:26:07 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/06 17:31:48 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,20 @@ void	print_sti(t_carriage *c, int **types, int value)
 int		op_sti(t_game *game, int place, t_carriage *c)
 {
 	int **types;
-	short val1;
+	int val1;
+	short val2;
+	val1 = 0;
+	val2 = 0;
 
 	if (!(types = get_arg_types(game, c)))
 		return (c->size);
 	if (types[1][0] == 1)
 		val1 = c->reg[types[1][1] - 1];
 	else if (types[1][0] == 2)
-		val1 = types[1][1];
+		val2 = types[1][1];
 	else
-		val1 = read_types(game->arena, place + types[1][1] % IDX_MOD, 2);
-	val1 += types[2][0] == 1 ? c->reg[types[2][1] - 1] : types[2][1];
+		val1 = read_types(game->arena, place + types[1][1] % IDX_MOD, 4);
+	val1 += val2 + types[2][0] == 1 ? c->reg[types[2][1] - 1] : types[2][1];
 	print_sti(c, types, val1);
 	val1 = val1 % IDX_MOD;
 	game->arena[ft_place(place + val1)] = (c->reg[types[0][1] - 1] >> 24) & 0xFF;

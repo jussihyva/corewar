f26350b9d9af72f2b4424167613a07013e0b772c
Juha Hakala <jhakala@c3r4p4.hive.fi>
2020-08-06 14:22:12 +0300

    fixed sti to take short (was int) for types[1-2][1], because dir_size and ind_size is 2, zjump return value fix

--- jhakala/test_dir/src/statements/op_zjmp.c
+++ jhakala/test_dir/src/statements/op_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:53:27 by jhakala           #+#    #+#             */
-/*   Updated: 2020/07/31 21:56:25 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/06 13:44:57 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		op_zjmp(t_game *game, int place, t_carriage *c)
 	int val1;
 
 	res = 0;
-	ft_memcpy(&res, &game->arena[place + 1], 2);
+	ft_memcpy(&res, &game->arena[ft_place(place + 1)], 2);
 	res = REV_S(res);
 	if (c->carry == 1)
 	{
@@ -29,7 +29,8 @@ int		op_zjmp(t_game *game, int place, t_carriage *c)
 			c->place += val1 + MEM_SIZE;
 		else
 			c->place += val1;
-		ft_printf(" P   %d | zjmp %d | (0x%04d) | OK\n", c->id, res, c->place);
+		ft_printf("PPP=%d\n", c->place);
+		ft_printf(" P   %d | zjmp %d | (0x%04d) | OK\n", c->id, res, ft_place(c->place));
 	}
 	else
 		ft_printf(" P   %d | zjmp %d | ERROR\n", c->id, res);

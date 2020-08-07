85f030719e23051d7b359f2702a7e7cde371faac
Juha Hakala <hakalan.juha@gmail.com>
2020-08-07 20:21:21 +0300

    working version of this madnes

--- jhakala/test_dir/src/game/game_utils.c
+++ jhakala/test_dir/src/game/game_utils.c
@@ -6,7 +6,7 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 18:31:16 by jhakala           #+#    #+#             */
-/*   Updated: 2020/08/06 14:17:32 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/07 20:07:37 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,18 @@ int read_types(char *arena, int from, int size)
 
 int		read_statement_code(t_carriage *c, t_game *game, int place)
 {
+	if (c->statement_code == -1)
+		c->statement_code = game->arena[place];
+	return (g_op_tab[c->statement_code - 1].f(game, place, c));
+//	return (read_game_param(game->arena, place, 1, NULL));
+}
+
+/*
+int		read_statement_code(t_carriage *c, t_game *game, int place)
+{
 	if ((c->statement_code == game->arena[place] ||
 		c->statement_code == -1) && game->arena[place] > 0 && game->arena[place] < 17)
 		return (g_op_tab[game->arena[place] - 1].f(game, place, c));
 	return (read_game_param(game->arena, place, 1, NULL));
 }
+*/

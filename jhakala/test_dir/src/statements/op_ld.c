ecb9f9398bcab505fe180ce796cc5c495b2dba28
Juha Hakala <hakalan.juha@gmail.com>
2020-08-08 15:40:39 +0300

    ld int to short cast

--- jhakala/test_dir/src/statements/op_ld.c
+++ jhakala/test_dir/src/statements/op_ld.c
@@ -6,7 +6,7 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:50:11 by jhakala           #+#    #+#             */
-/*   Updated: 2020/08/08 14:31:30 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/08 15:36:33 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int		op_ld(t_game *game, int place, t_carriage *c)
 
 	if (!(types = get_arg_types(game, c)))
 		return (c->size);
-//	ft_printf(" P   %d | ld", c->id);
+	ft_printf(" P   %d | ld", c->id);
 	if (types[0][0] == 2)
 		c->reg[types[1][1] - 1] = types[0][1];
 	else
-		c->reg[types[1][1] - 1] = read_types(game->arena, place + (types[0][1] % IDX_MOD), REG_SIZE);
+		c->reg[types[1][1] - 1] = read_types(game->arena, place + ((short)types[0][1] % IDX_MOD), REG_SIZE);
 	if (c->reg[types[1][1] - 1] == 0)
 		c->carry = 1;
 	else
 		c->carry = 0;
-//	ft_printf(" %d r%d | %d | carry = %d\n", c->reg[types[1][1] - 1], types[1][1],
-//			c->reg[types[1][1] - 1], c->carry);
+	ft_printf(" %d r%d | %d | carry = %d\n", c->reg[types[1][1] - 1], (short)types[1][1],
+			c->reg[types[1][1] - 1], c->carry);
 	return (read_game_param(game->arena, place, c->size, types));
 }

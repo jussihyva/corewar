f1904dd26c33bf729ccc80e0a9fded1d0a17329f
Juhani Kauppi <jkauppi@student.hive.fi>
2020-08-08 21:47:40 +0300

    corewar tester implemented.

--- jhakala/test_dir/mod_compare.sh
+++ jhakala/test_dir/mod_compare.sh
@@ -1,6 +1,7 @@
 echo $1
 echo $2
-./corewar -d $1  $2 | egrep "^0x0" > ref.log
-./prog -dump $1  $2 | egrep "^0x0" > we.log
+../../ProjectInputInformation/corewar -d $1  $2 | egrep "^0x0" > ref.log
+../../jhakala/test_dir/prog -dump $1  $2 | egrep "^0x0" > we.log
 diff we.log ref.log
+exit 2000
 

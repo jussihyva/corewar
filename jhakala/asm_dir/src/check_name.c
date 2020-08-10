febc3a6b003cdcd4e0abff7d05247b072fee19a6
Juha Hakala <jhakala@c3r4p6.hive.fi>
2020-08-10 19:33:37 +0300

    norm erro fixes and final ; addition to parseutils

--- jhakala/asm_dir/src/check_name.c
+++ jhakala/asm_dir/src/check_name.c
@@ -6,25 +6,39 @@
 /*   By: jhakala <jhakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 09:28:42 by jhakala           #+#    #+#             */
-/*   Updated: 2020/07/27 16:03:32 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/10 19:06:58 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int		ft_name_error(char* str)
+int		ft_name_error(char *str)
 {
 	ft_printf("Invalid file end: '%s'\n", str);
 	return (0);
 }
 
+void	free_tmp_name(char *name, char *tmp)
+{
+	free(name);
+	free(tmp);
+}
+
+char	*ft_name(char *tmp, char *end, char *name)
+{
+	name = ft_strjoin(tmp, end);
+	ft_printf("Writing to file :\"%s\"\n", name);
+	return (name);
+}
+
 int		check_name(int *fd, char *str, int *op, char *end)
 {
-	int i;
-	int j;
-	char *tmp;
-	char *name;
-	
+	int		i;
+	int		j;
+	char	*tmp;
+	char	*name;
+
+	name = NULL;
 	if ((j = ft_strlen(str) - 2) > 0 && str[j] == '.' && str[j + 1] == 's')
 	{
 		i = op[22] == 1 ? j - 2 : 0;
@@ -38,10 +52,9 @@ int		check_name(int *fd, char *str, int *op, char *end)
 			i--;
 		}
 		ft_strlcat((tmp = ft_strnew(j - i + 5)), &(*(str + i)), j - i + 1);
-		*fd = open((name = ft_strjoin(tmp, end)), O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		ft_printf("Writing to file :\"%s\"\n", name);
-		free(tmp);
-		free(name);
+		*fd = open(ft_name(tmp, end, name), O_WRONLY | O_CREAT | O_TRUNC,
+				0644);
+		free_tmp_name(name, tmp);
 		return (*fd > 0 ? 1 : 0);
 	}
 	return (ft_name_error(str));
--- jhakala/asm_dir/src/op.c
+++ jhakala/asm_dir/src/op.c
@@ -6,7 +6,7 @@
 /*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:43:01 by zaz               #+#    #+#             */
-/*   Updated: 2020/07/27 16:22:01 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/10 19:14:04 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,17 @@ t_op	g_op_tab[17] =
 	{"st", 2, 2, {T_REG, T_IND | T_REG}, 3, 1, 4},
 	{"add", 3, 3, {T_REG, T_REG, T_REG}, 4, 1, 4},
 	{"sub", 3, 3, {T_REG, T_REG, T_REG}, 5, 1, 4},
-	{"and", 3, 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG}, 6, 1, 4},
-	{"or", 2, 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 7, 1, 4},
-	{"xor", 3, 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 8, 1, 4},
+	{"and", 3, 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR, T_REG},
+		6, 1, 4},
+	{"or", 2, 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG},
+		7, 1, 4},
+	{"xor", 3, 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG},
+		8, 1, 4},
 	{"zjmp", 4, 1, {T_DIR}, 9, 0, 2},
 	{"fork", 4, 1, {T_DIR}, 12, 0, 2},
-	{"lldi", 4, 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 1, 2},	
+	{"lldi", 4, 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 1, 2},
 	{"lld", 3, 2, {T_DIR | T_IND, T_REG}, 13, 1, 4},
-	{"lfork", 5, 1, {T_DIR}, 15, 0, 2},	
+	{"lfork", 5, 1, {T_DIR}, 15, 0, 2},
 	{"aff", 3, 1, {T_REG}, 16, 1, 4},
 	{0, 0, 0, {0}, 0, 0, 0}
 };

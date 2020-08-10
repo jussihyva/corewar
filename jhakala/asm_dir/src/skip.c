156ad4df6b67f93a07004d84442a5d143f031e76
Juha Hakala <hakalan.juha@gmail.com>
2020-08-10 19:33:37 +0300

    norm erro fixes and final ; addition to parseutils

--- jhakala/asm_dir/src/skip.c
+++ jhakala/asm_dir/src/skip.c
@@ -6,7 +6,7 @@
 /*   By: hopham <hopham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/20 01:08:26 by jhakala           #+#    #+#             */
-/*   Updated: 2020/08/10 15:53:53 by hopham           ###   ########.fr       */
+/*   Updated: 2020/08/10 18:58:18 by jhakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int		skip_to(char *str, char c, int i)
 		j++;
 	while (str[j])
 	{
-		if (ft_strchr(COMMENT_CHAR, str[i]) || str[j] == '\n') // str[j] == COMMENT_CHAR
+		if (ft_strchr(COMMENT_CHAR, str[i]) || str[j] == '\n')
 			break ;
 		if (str[j] != ' ' && str[j] != '	')
 			return (-1);
@@ -47,7 +47,7 @@ int		skip_to(char *str, char c, int i)
 int		valid_end(char *str, int i)
 {
 	i = skip_whitespace(str, i);
-	if (ft_strchr(COMMENT_CHAR, str[i]) || str[i] == '\0' || str[i] == '\n') // str[i] == COMMENT_CHAR
+	if (ft_strchr(COMMENT_CHAR, str[i]) || str[i] == '\0' || str[i] == '\n')
 		return (1);
 	return (0);
 }

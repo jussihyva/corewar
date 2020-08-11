48b11ca68b61d49b15b7b4420dfcd9d65d9cf8db
HoangPham <pham.hoang1591@gmail.com>
2020-08-11 14:51:40 +0300

    working on norminette test_dir, still some comments needed to be fixed

--- jhakala/test_dir/src/statements/op_lld.c
+++ jhakala/test_dir/src/statements/op_lld.c
@@ -6,7 +6,7 @@
 /*   By: hopham <hopham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:54:42 by jhakala           #+#    #+#             */
-/*   Updated: 2020/08/10 20:55:49 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/11 14:23:28 by hopham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,15 @@ int		op_lld(t_game *game, int place, t_carriage *c)
 	if (types[0][0] == 2)
 		c->reg[types[1][1] - 1] = types[0][1];
 	else
-		c->reg[types[1][1] - 1] = read_types(game->arena, place + types[0][1], REG_SIZE);
+		c->reg[types[1][1] - 1] =
+						read_types(game->arena, place + types[0][1], REG_SIZE);
 	if (c->reg[types[1][1] - 1] == 0)
 		c->carry = 1;
 	else
 		c->carry = 0;
 	if (game->print)
-		ft_printf(" P   %d | lld %d r%d | %d | carry = %d\n", c->id, c->reg[types[1][1] - 1], types[1][1],
-				c->reg[types[1][1] - 1], c->carry);
+		ft_printf(" P   %d | lld %d r%d | %d | carry = %d\n",
+					c->id, c->reg[types[1][1] - 1], types[1][1],
+					c->reg[types[1][1] - 1], c->carry);
 	return (read_game_param(game, place, c->size, types));
 }

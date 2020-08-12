390879bae87b92ee4900a2602b36e5a199bb94d2
HoangPham <pham.hoang1591@gmail.com>
2020-08-12 16:26:11 +0300

    fix norm op_sti.c

--- src_corewar/statements/op_sti.c
+++ src_corewar/statements/op_sti.c
@@ -6,7 +6,7 @@
 /*   By: hopham <hopham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:54:08 by jhakala           #+#    #+#             */
-/*   Updated: 2020/08/12 16:14:27 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/12 16:24:58 by hopham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,29 +36,29 @@ void	print_sti(t_game *game, t_carriage *c, int **types, int value)
 
 int		op_sti(t_game *game, int place, t_carriage *c)
 {
-	int		**types;
+	int		**ty;
 	int		val1;
 	short	val2;
 
 	val1 = 0;
 	val2 = 0;
-	if (!(types = get_arg_types(game, c)))
+	if (!(ty = get_arg_types(game, c)))
 		return (c->size);
-	if (types[1][0] == 1)
-		val1 = c->reg[types[1][1] - 1];
-	else if (types[1][0] == 2)
-		val2 = types[1][1];
+	if (ty[1][0] == 1)
+		val1 = c->reg[ty[1][1] - 1];
+	else if (ty[1][0] == 2)
+		val2 = ty[1][1];
 	else
-		val1 = read_types(game->arena, place + types[1][1] % IDX_MOD, 4);
-	val1 += val2 + (types[2][0] == 1 ? c->reg[types[2][1] - 1] : (short)types[2][1]);
-	print_sti(game, c, types, val1);
+		val1 = read_types(game->arena, place + ty[1][1] % IDX_MOD, 4);
+	val1 += val2 + (ty[2][0] == 1 ? c->reg[ty[2][1] - 1] : (short)ty[2][1]);
+	print_sti(game, c, ty, val1);
 	val1 %= IDX_MOD;
-	game->arena[ft_place(place + val1)] = (c->reg[types[0][1] - 1] >> 24)
+	game->arena[ft_place(place + val1)] = (c->reg[ty[0][1] - 1] >> 24)
 		& 0xFF;
-	game->arena[ft_place(place + val1 + 1)] = (c->reg[types[0][1] - 1] >> 16)
+	game->arena[ft_place(place + val1 + 1)] = (c->reg[ty[0][1] - 1] >> 16)
 		& 0xFF;
-	game->arena[ft_place(place + val1 + 2)] = (c->reg[types[0][1] - 1] >> 8)
+	game->arena[ft_place(place + val1 + 2)] = (c->reg[ty[0][1] - 1] >> 8)
 		& 0xFF;
-	game->arena[ft_place(place + val1 + 3)] = c->reg[types[0][1] - 1] & 0xFF;
-	return (read_game_param(game, place, c->size, types));
+	game->arena[ft_place(place + val1 + 3)] = c->reg[ty[0][1] - 1] & 0xFF;
+	return (read_game_param(game, place, c->size, ty));
 }

d270a126d6be2661179ea355bcefc643aa3b034a
Juha Hakala <hakalan.juha@gmail.com>
2020-08-12 15:00:13 +0300

    head Makefile

--- vm_dir/mod_compare.sh
+++
@@ -1,7 +0,0 @@
-echo $1
-echo $2
-../../ProjectInputInformation/corewar -d $1  $2 | egrep "^0x0" > ref.log
-../../jhakala/test_dir/prog -dump $1  $2 | egrep "^0x0" > we.log
-diff we.log ref.log
-exit 2000
-

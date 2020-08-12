fa564e948774b9d4931c7378aa42317b1e02cb2d
Juhani Kauppi <jkauppi@student.hive.fi>
2020-08-12 14:42:52 +0300

    corewar_tester/corewar_tester.py improved to verify 1-4 champions

--- corewar_tester/compare.sh
+++ corewar_tester/compare.sh
@@ -1,4 +1,4 @@
-./corewar -d $2  $3 | egrep "^0x0" > ref.log
-$1 -d $2  $3 | egrep "^0x0" > we.log
+./corewar -d $2  $3 $4 $5 $6 | egrep "^0x0" > ref.log
+$1 -d $2  $3 $4 $5 $6 | egrep "^0x0" > we.log
 diff we.log ref.log
 exit 0

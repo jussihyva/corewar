a761d34a58347a7355d89b9d98809a8d5e87af73
HoangPham <pham.hoang1591@gmail.com>
2020-08-12 17:15:19 +0300

    fix norm op_sti.c

--- src_corewar/statements/op_sti.c
+++ src_corewar/statements/op_sti.c
@@ -6,7 +6,7 @@
 /*   By: hopham <hopham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/23 11:54:08 by jhakala           #+#    #+#             */
-/*   Updated: 2020/08/12 17:04:32 by jhakala          ###   ########.fr       */
+/*   Updated: 2020/08/12 17:14:26 by hopham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ void	print_sti(t_game *game, t_carriage *c, int **types, int value)
 		else if (types[1][0] == 2)
 			ft_printf(" %d", (short)types[1][1]);
 		else
-			ft_printf(" %d", read_types(game->arena, c->place + (short)types[1][1] % IDX_MOD, 4));
-//			ft_printf(" %d", types[1][1]);
+			ft_printf(" %d", read_types(game->arena,
+								c->place + (short)types[1][1] % IDX_MOD, 4));
 		if (types[2][0] == 1)
 			ft_printf(" %d", c->reg[types[2][1] - 1]);
 		else
@@ -35,7 +35,7 @@ void	print_sti(t_game *game, t_carriage *c, int **types, int value)
 	}
 }
 
-int		op_sti(t_game *game, int place, t_carriage *c)
+int		op_sti(t_game *g, int place, t_carriage *c)
 {
 	int		**ty;
 	int		val1;
@@ -43,23 +43,23 @@ int		op_sti(t_game *game, int place, t_carriage *c)
 
 	val1 = 0;
 	val2 = 0;
-	if (!(ty = get_arg_types(game, c)))
+	if (!(ty = get_arg_types(g, c)))
 		return (c->size);
 	if (ty[1][0] == 1)
 		val1 = c->reg[ty[1][1] - 1];
 	else if (ty[1][0] == 2)
 		val2 = (short)ty[1][1];
 	else
-		val1 = read_types(game->arena, place + ((short)(ty[1][1]) % IDX_MOD), 4);
+		val1 = read_types(g->arena, place + ((short)(ty[1][1]) % IDX_MOD), 4);
 	val1 += val2 + (ty[2][0] == 1 ? c->reg[ty[2][1] - 1] : (short)ty[2][1]);
-	print_sti(game, c, ty, val1);
+	print_sti(g, c, ty, val1);
 	val1 %= IDX_MOD;
-	game->arena[ft_place(place + val1)] = (c->reg[ty[0][1] - 1] >> 24)
+	g->arena[ft_place(place + val1)] = (c->reg[ty[0][1] - 1] >> 24)
 		& 0xFF;
-	game->arena[ft_place(place + val1 + 1)] = (c->reg[ty[0][1] - 1] >> 16)
+	g->arena[ft_place(place + val1 + 1)] = (c->reg[ty[0][1] - 1] >> 16)
 		& 0xFF;
-	game->arena[ft_place(place + val1 + 2)] = (c->reg[ty[0][1] - 1] >> 8)
+	g->arena[ft_place(place + val1 + 2)] = (c->reg[ty[0][1] - 1] >> 8)
 		& 0xFF;
-	game->arena[ft_place(place + val1 + 3)] = c->reg[ty[0][1] - 1] & 0xFF;
-	return (read_game_param(game, place, c->size, ty));
+	g->arena[ft_place(place + val1 + 3)] = c->reg[ty[0][1] - 1] & 0xFF;
+	return (read_game_param(g, place, c->size, ty));
 }
